c6ea9b6bc7aeefe15cfca9baf1ef3251c55bb1e6
zxy51320 <78584947+zxy51320@users.noreply.github.com>
2025-07-18 17:27:00 -0400

    update

--- Pages/dashboard_page.py
+++ Pages/dashboard_page.py
@@ -1,36 +1,57 @@
 # pages/dashboard_page.py
 import time
 from playwright.sync_api import Page
+import logging
 
 class DashboardPage:
     def __init__(self, page: Page):
         self.page = page
 
     def search_mid(self, mid: str):
-        self.page.get_by_role("textbox", name="Filter...").click()
-        self.page.get_by_role("textbox", name="Filter...").fill(mid)
-        self.page.get_by_role("tab", name="Merchant", exact=True).click()
-        self.page.get_by_role("button", name="Go To").click()
-
-    def navigate_to_statements(self):
-        self.page.get_by_role("tab", name="TSYS").click()
-        self.page.get_by_role("tab", name="Reports").click()
         try:
-            self.page.get_by_test_id("MerchantDetails-WaitSpinner").locator("svg").nth(3).click()
-            time.sleep(1)  # 等待内容加载
-            self.page.get_by_test_id("MerchantDetails-WaitSpinner").locator("svg").nth(3).click()
+            self.page.get_by_role("textbox", name="Filter...").click()
+            self.page.get_by_role("textbox", name="Filter...").fill(mid)
+            self.page.get_by_role("tab", name="Merchant", exact=True).click(timeout=3000)
+            self.page.get_by_role("button", name="Go To").click(timeout=3000)
         except Exception as e:
-            pass
-        self.page.get_by_role("tab", name="Statements - MXC").click()
+            logging.error(f"Can not find MID {mid}")
+            raise e
 
-    def download_pdf(self, save_path: str):
-        self.page.locator("td:nth-child(8)").first.click()
-        self.page.get_by_role("button", name="Summary").click()
-        self.page.get_by_role("option", name="Key 3").click()
+    def download_pdf(self,mid: str,save_path: str):
+        first_four = mid[:4]
+        #815219362887
+        #56317155030542
+        #554402040703142
+        #8739781940608005
+        if first_four == "8739" or first_four == "5631":
+            self.page.get_by_role("tab", name="TSYS").click()
+            logging.info(f"MID: {mid} for TSYS")
+        elif first_four == "8152":
+            self.page.get_by_role("tab", name="Fiserv North").click()
+            logging.info(f"MID: {mid} for Fiserv North")
+        elif first_four == "5544":
+            self.page.get_by_role("tab", name="Fiserv - Omaha").click()
+            logging.info(f"MID: {mid} for Fiserv - Omaha")
+        self.page.get_by_role("tab", name="Reports").click()
+        while not self.page.get_by_role("tab", name="Statements").is_visible():
+            self.page.get_by_test_id("MerchantDetails-WaitSpinner").locator("svg").nth(3).click()
+        self.page.get_by_role("tab", name="Statements").click()
+        try:
+            self.page.locator(".src-components-dynamic-grid-styles--cell").first.click(timeout=3000)
+        except Exception as e:
+            logging.error(f"No statements found for MID {mid}")
+            raise e
+        try:
+            self.page.get_by_role("button", name="Summary").click(timeout=3000)
+            self.page.get_by_role("option", name="Key 3").click(timeout=3000)
+            logging.info(f"Key 3")
+        except Exception as e:
+            logging.info(f"Lagacy")
         with self.page.expect_download() as download_info:
             self.page.get_by_role("button", name="PDF").click()
         download = download_info.value
         download.save_as(save_path)
-        #self.page.keyboard.press("Escape")
-        self.page.get_by_role("button", name="").click() # 关闭下载对话框
-        time.sleep(1)  # 等待下载完成
+        self.page.keyboard.press("Escape") # 关闭下载对话框
+        #self.page.get_by_role("button", name="").click() 
+        time.sleep(2)  # 控制下载频率
+        logging.info(f"Downloaded statement for MID {mid}")
--- download_statement.py
+++ download_statement.py
@@ -13,27 +13,41 @@ def load_mids(csv_path: str):
 
 def statement_download():
     mids = load_mids("TestData/Merchant Open Account (list_of_open_account).csv")
+    if not mids:
+        logging.error("No MIDs found in the CSV file.")
+        return
     username = "Brianl@zbspos.com"
     password = "1990100900pW@"
 
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
-        context = browser.new_context(accept_downloads=True, viewport={"width": 1440, "height": 900})
+        context = browser.new_context(accept_downloads=True, viewport={"width": 1440, "height": 800})
         page = context.new_page()
-
+        page.set_default_timeout(15000)
         login_page = LoginPage(page)
         dashboard = DashboardPage(page)
 
         login_page.goto()
         login_page.login(username, password)
-
+        failures = []
         for mid in mids:
-            print(f"正在处理 MID: {mid}")
-            dashboard.search_mid(mid)
-            dashboard.navigate_to_statements()
-            dashboard.download_pdf(f"statements/{mid}.pdf")
-
+            try:
+                logging.info(f"Started - MID: {[mid]}")
+                dashboard.search_mid(mid)
+                dashboard.download_pdf(mid,f"statements/{mid}.pdf")
+            except Exception as e:
+                failures.append(mid)
+                logging.error(f"Error processing MID {mid}: {e}")
+        logging.info(f"Fallures: {failures}")
         context.close()
         browser.close()
 
-statement_download()
+if __name__ == "__main__":
+    # Configure logging
+    log_file = f"log.log"
+    logging.basicConfig(
+        filename=log_file,
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+    statement_download()
